cceece3e289251df9b78f80068029e4e88f01e7e
Eden Dupont <eg7eg7@gmail.com>
2018-05-02 22:27:47 +0300

    Initial commit

---
+++ ID_204808596/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+import ac.il.afeka.Submission.Submission;
+import ac.il.afeka.fsm.DFSM;
+
+public class Main implements Submission, Assignment3 {
+
+	@Override
+	public List<String> submittingStudentIds() {
+		return Arrays.asList("204808596", "334018009");
+	}
+
+	@Override
+	public DFSM convert(String aNDFSMencoding) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
