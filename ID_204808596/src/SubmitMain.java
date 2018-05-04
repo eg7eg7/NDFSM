cbefd2873b17406dee3d5c1fb403cc89af91a1f4
Eden Dupont <eg7eg7@gmail.com>
2018-05-04 20:16:57 +0300

    settings

--- ID_204808596/src/SubmitMain.java
+++ ID_204808596/src/SubmitMain.java
@@ -9,13 +9,12 @@ import ac.il.afeka.fsm.DFSM;
 import ac.il.afeka.fsm.NDFSM;
 
 public class SubmitMain implements Submission, Assignment3 {
-	private static int DFSMCount=0;
-		
-	public SubmitMain()
-	{
+	private static int DFSMCount = 0;
+
+	public SubmitMain() {
 		DFSMCount++;
 	}
-	
+
 	@Override
 	public List<String> submittingStudentIds() {
 		return Arrays.asList("204808596", "334018009");
@@ -41,17 +40,14 @@ public class SubmitMain implements Submission, Assignment3 {
 		SubmitMain submitExample = new SubmitMain();
 		String aNDFSMencoding = "0 1 2 3 4 5/a b/0, a, 2;0, b, 3;0,, 1;1, a, 2;1, b, 3;1, a, 4;2, a, 2;2, b, 3;2, b, 5;3,, 0;4, a, 1;4, a, 4;4, b, 5;5, a, 4/0/2 5";
 		submitExample.convert(aNDFSMencoding);
-		
-		
+
 		SubmitMain submitExample2 = new SubmitMain();
 		String aNDFSMencoding2 = "0 1 2/a b/0, a, 0;0, a, 1;0, b, 2;1, a, 0;1, b, 1;2, b, 0;2, b, 1/0/2";
 		submitExample2.convert(aNDFSMencoding2);
-		
+
 		SubmitMain submitExample3 = new SubmitMain();
 		String aNDFSMencoding3 = "1 2 3 4/a b c/1, a, 2;1, c, 4;2,, 1;2, b, 3;3, a, 2;4, c, 3;4,, 3/1/3";
 		submitExample3.convert(aNDFSMencoding3);
-		
-		
 
 	}
 }
